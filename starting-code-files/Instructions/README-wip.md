43e3e60e0fc69c226af5cbd5dca4d0f58e726c18
Michael Murphy <mikemurf@bigpond.net.au>
2022-03-29 08:33:56 +1100

    final load for assessment

---
+++ starting-code-files/Instructions/README-wip.md
@@ -0,0 +1,111 @@
+![Map Description automatically
+generated](media/76d508fda657417b998fc2d4101249fe.png)
+
+\#\#Visualising Earthquake Data with Leaflet
+
+\#\#\#\#\# UCLA reference goes here
+
+\#\# Requirements
+
+The summary requirements for this homework are as follows: -
+
+“ Welcome to the United States Geological Survey, or USGS for short! The USGS is
+responsible for providing scientific data about natural hazards, the health of
+our ecosystems and environment; and the impacts of climate and land-use change.
+Their scientists develop new methods and tools to supply timely, relevant, and
+useful information about the Earth and its processes. As a new hire, you will be
+helping them out with an exciting new project!
+
+The USGS is interested in building a new set of tools that will allow them to
+visualise their earthquake data. They collect a massive amount of data from all
+over the world each day, but they lack a meaningful way of displaying it. Their
+hope is that being able to visualise their data will allow them to better
+educate the public and other government organisations (and hopefully secure more
+funding..) on issues facing our planet.
+
+Your first task is to visualise an earthquake data set ……from the [USGS GeoJSON
+Feed](http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php) page.
+
+The USGS wants you to plot a second data set on your map to illustrate the
+relationship between tectonic plates and seismic activity. You will need to pull
+in a second data set and visualise it alongside your original set of data. Data
+on tectonic plates can be found at <https://github.com/fraxen/tectonicplates>. “
+
+The full details of the requirements for this homework can be found in the
+directories / files detailed below.
+
+\#\#The “leaflet-challenge/starting-code-files/instructions” directory contains
+the remaining directories / files submitted for assessment:
+
+-   “README” and “Leaflet_Grading_Rubric” files containing the detailed
+    instructions and grading assessment for the assignment,
+
+-   “Images” directory containing the provided images of the required outputs
+    for the assignment and the header image courtesy of UCLA,
+
+-   “index-provided.html”: the HTML file that was provided as part of the
+    starting code for the assignment,
+
+-   “screenshots” directory containing screenshots of the outputs produced for
+    the visualisations,
+
+-   “index.html”: the HTML file that drives the “US Geological Survey:
+    Earthquakes Data Visualisations for the Last 7 Days”,
+
+-   “static/js” directory that contains the “config.sys” file and the “logic.js”
+    for the application invoked by the HTML,
+
+    (NOTE: The “config.sys” file uses the default public key produced by
+    Mapbox.)
+
+-   “static/css” directory that contains the “style” CSS file used by the HTML,
+
+-   “README-wip is a work in process README file produced with the Writage Word
+    extension and used as the basis for the README file for the github
+    repository,
+
+\#\# Author: Mike Murphy
+
+\#\# Tackling the Assignment
+
+\#\#\#The assignment was tackled by combining the two USGS requirements and
+designing a solution to: -
+
+-   visualise an earthquake data set,
+
+-   to include popups providing information about the earthquake when a marker
+    is clicked,
+
+-   to include a number of base maps to choose from,
+
+-   to separate two different data sets into overlays that can be turned on and
+    off independently,
+
+-   to add layer controls to the map.
+
+\#\#\# Screenshots of the various outputs follow.
+
+![Map Description automatically
+generated](media/ffc5b22c3d645a7e250ed8925d05f3c3.png)
+
+![Map Description automatically
+generated](media/378902fd6afa08a600840bc5d6ea4cff.png)
+
+![Map Description automatically
+generated](media/6e283f6c2c1c8ff0a28976ee864e9d41.png)
+
+![Graphical user interface Description automatically
+generated](media/ec76b178081f778de46369c4f932f325.png)
+
+![Map Description automatically
+generated](media/63dd1d7b36264c8d235f2d36d8bae9df.png) ![Map Description
+automatically generated](media/9a6ab309734a1f8eb11eb716ac895e40.png)
+
+![Map Description automatically
+generated](media/5a7425fdffe4429315f3b5c8a60e32a0.png)
+
+![Map Description automatically
+generated](media/c6896a0bc4f2c4302d72f8a79fad3ab6.png)
+
+![Map Description automatically
+generated](media/f0d990fe23dbe5f4a60740e38b2482dc.png)
---
+++ starting-code-files/Instructions/static/js/logic.js
@@ -0,0 +1,133 @@
+// Define earthquakes, tectonic plates, GeoJSON URL variables
+var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var tectonicplatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+// Create layerGroups
+var earthquakes = L.layerGroup();
+var tectonicplates = L.layerGroup();
+
+// Adding tile layers
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+maxZoom: 18,
+id: "mapbox.satellite",
+accessToken: API_KEY
+});
+
+var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/light-v10",
+accessToken: API_KEY
+});
+
+var outdoorsMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+tileSize: 512,
+maxZoom: 18,
+zoomOffset: -1,
+id: "mapbox/outdoors-v11",
+accessToken: API_KEY
+});
+
+// Define a baseMaps object to hold the base layers
+var baseMaps = {
+"Satellite Map": satelliteMap,
+"Grayscale Map": grayscaleMap,
+"Outdoors Map": outdoorsMap,
+};
+
+// Create overlay object to hold the overlay layer
+var overlayMaps = {
+"Earthquakes": earthquakes,
+"Tectonic Plates": tectonicplates
+};
+
+// Create map giving it the satelliteMap and earthquakes layers to display on load
+var myMap = L.map("mapid", {
+center: [
+37.09, -95.71
+],
+zoom: 2,
+layers: [satelliteMap, earthquakes]
+});
+
+// Create layer control, pass in baseMaps and overlayMaps, add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+collapsed: false
+}).addTo(myMap);
+
+//Grab data with D3
+d3.json(earthquakesURL, function(earthquakeData) {
+// Determine marker size by magnitude of earthquake
+function markerSize(magnitude) {
+return magnitude * 4;
+};
+// Determine marker color by depth of earthquake in kilometres
+function chooseColor(depth) {
+switch(true) {
+	case depth > 90:
+	return "red";
+	case depth > 70:
+	return "orangered";
+	case depth > 50:
+	return "orange";
+	case depth > 30:
+	return "gold";
+	case depth > 10:
+	return "yellow";
+	default:
+	return "lightgreen";
+}
+}
+// Create a GeoJSON layer containing the features array
+// Each feature is a popup describing place and time of the earthquake
+L.geoJSON(earthquakeData, {
+pointToLayer: function (feature, latlng) {
+	return L.circleMarker(latlng, 
+	// Set the style of the markers based on properties.mag
+	{
+		radius: markerSize(feature.properties.mag),
+		fillColor: chooseColor(feature.geometry.coordinates[2]),
+		fillOpacity: 0.7,
+		color: "black",
+		stroke: true,
+		weight: 0.5
+	}
+	);
+},
+onEachFeature: function(feature, layer) {
+	layer.bindPopup("<h3>Location: " + feature.properties.place + "</h3><hr><p>Date: "
+	+ new Date(feature.properties.time) + "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+}
+}).addTo(earthquakes);
+// Send earthquakes layer to the createMap function
+earthquakes.addTo(myMap);
+
+// Load tectonic plate data from tectonicplatesURL
+d3.json(tectonicplatesURL, function(data) {
+L.geoJSON(data, {
+	color: "orange",
+	weight: 2
+}).addTo(tectonicplates);
+tectonicplates.addTo(myMap);
+});
+
+// Add legend
+var legend = L.control({position: "bottomright"});
+legend.onAdd = function() {
+	var div = L.DomUtil.create("div", "info legend"),
+	depth = [0, 10, 30, 50, 70, 90];
+	div.innerHTML += "<h3 style='text-align: center'>Depth: Kms</h3>"
+	for (var i =0; i < depth.length; i++) {
+	div.innerHTML += 
+	'<i style="background:' + chooseColor(depth[i] + 1) + '"></i> ' +
+		depth[i] + (depth[i + 1] ? '&ndash;' + depth[i + 1] + '<br>' : '+');
+	}
+	return div;
+	};
+	legend.addTo(myMap);
+	});
+
